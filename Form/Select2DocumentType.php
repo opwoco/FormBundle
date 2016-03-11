2cfb27f0ec8e27c3fd9390bb7173625d1abd33c1
Alsatian67 <romain6772@gmail.com>
2016-03-11 12:53:43 +0100

    Update Select2DocumentType.php

--- Form/Select2DocumentType.php
+++ Form/Select2DocumentType.php
@@ -1,54 +1,10 @@
 <?php
 namespace Alsatian\FormBundle\Form;
 
-use Symfony\Component\Form\AbstractType;
-
-use Symfony\Component\OptionsResolver\Options;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
 use Doctrine\Bundle\MongoDBBundle\Form\Type\DocumentType;
 
-class Select2DocumentType extends AbstractType
+class Select2DocumentType extends AbstractSelect2Type
 {
-    private $router = null;
-    private $inputClass;
-    
-    public __construct($inputClass)
-    {
-        $this->inputClass = $inputClass;
-    }
-        
-    public function setRouter($router)
-    {
-        $this->router = $router;
-    }
-   
-    public function configureOptions(OptionsResolver $resolver)
-    {      
-        $resolver->setDefault('route', null);
-        $resolver->setDefault('attr', function(Options $options, $attr){
-            if($this->inputClass){
-                $attr['class'] = $this->inputClass;
-            }
-            
-            if($option['route']){
-                if(!$this->router){$this->setRouter();}
-                
-                $attr['data-ajax--url']=$this->router->generate($options['route']);
-            }
-            
-            return $attr;
-        });
-        
-        $resolver->setDefault('choices', function(Options $options, $choices)){
-            if($option['route']){
-                $choices = array();
-            };
-            
-            return $choices;
-        }
-    }
-    
     public function getParent()
     {
         return DocumentType::class;
