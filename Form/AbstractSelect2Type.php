2f586a8fb87a31dd0a2a21988723c27e2b8efc97
Alsatian67 <romain6772@gmail.com>
2016-03-11 13:21:53 +0100

    Update AbstractSelect2Type.php

--- Form/AbstractSelect2Type.php
+++ Form/AbstractSelect2Type.php
@@ -6,26 +6,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 abstract class AbstractSelect2Type extends AbstractRoutableType
 {
-    private $default_attr_class;
-    
-    public __construct($default_attr_class)
-    {
-        $this->default_attr_class = $default_attr_class;
-    }
-    
     public function configureOptions(OptionsResolver $resolver)
     {  
         if($option['route']){
             $resolver->setDefault('choices', array());
         };
-        
-        if($this->default_attr_class){
-            $resolver->setDefault('attr', function(Options $options, $attr){
-            
-                $attr['class'] = $this->default_attr_class;
-                                
-                return $attr;
-            });
-        }
     }
 }
