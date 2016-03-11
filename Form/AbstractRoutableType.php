e17cb102d514904400cc31f84ed3e0b4536aef43
Alsatian67 <romain6772@gmail.com>
2016-03-11 13:01:22 +0100

    Create AbstractRoutableType.php

---
+++ Form/AbstractRoutableType.php
@@ -0,0 +1,32 @@
+<?php
+namespace Alsatian\FormBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+abstract class AbstractRoutableType extends AbstractType
+{
+    private $router;
+        
+    public function setRouter($router)
+    {
+        $this->router = $router;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {  
+        $resolver->setDefault('route', false);
+                
+        if($option['route']){
+            $resolver->setDefault('attr', function(Options $options, $attr){
+
+				$this->setRouter();				
+				$attr['data-ajax--url']=$this->router->generate($options['route']);
+
+                return $attr;
+            });
+        }
+    }
+}
