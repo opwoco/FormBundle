56f1a9e1261a764d01d1cb418ca1eef223b34fcb
Alsatian67 <romain6772@gmail.com>
2016-03-16 14:55:50 +0100

    Update and rename Select2EntityType.php to ExtensibleEntityType.php

--- Form/ExtensibleEntityType.php
+++ Form/ExtensibleEntityType.php
@@ -3,7 +3,7 @@ namespace Alsatian\FormBundle\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class Select2EntityType extends AbstractExtensibleChoicesType
+class ExtensibleEntityType extends AbstractExtensibleChoicesType
 {
     public function getParent()
     {
