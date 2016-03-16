cf070526ec064c5d8acc24b9fb7052a90ec4204b
Alsatian67 <romain6772@gmail.com>
2016-03-16 14:54:39 +0100

    Update AbstractSelect2Type.php

--- Form/AbstractSelect2Type.php
+++ Form/AbstractSelect2Type.php
@@ -4,12 +4,12 @@ namespace Alsatian\FormBundle\Form;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-abstract class AbstractSelect2Type extends AbstractRoutableType
+abstract class AbstractExtensibleChoicesType extends AbstractRoutableType
 {
     public function configureOptions(OptionsResolver $resolver)
     {  
         parent::configureOptions($resolver);
         
-        $resolver-> replaceDefaults(array('choices'=>array()));
+        $resolver->replaceDefaults(array('choices'=>array()));
     }
 }
